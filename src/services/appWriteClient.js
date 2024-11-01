4b5555f464a33fbcbd0e42a69a17e4aff7acaa3e
acem702 <55085012+acem702@users.noreply.github.com>
2024-11-01 09:16:22 +0000

    Football

--- src/services/appWriteClient.js
+++ src/services/appWriteClient.js
@@ -3,8 +3,8 @@ import { Client, Databases, Storage } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint('http://localhost/v1') // Your Appwrite endpoint
-    .setProject('YOUR_PROJECT_ID'); // Your Project ID
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite endpoint
+    .setProject('football'); // Your Project ID
 
 export const databases = new Databases(client);
 export const storage = new Storage(client);
